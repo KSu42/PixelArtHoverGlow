1488a7ce914a5b6647a6e4fdc9038338b2e9ce72
Kevin Sudor <ksudor42@gmail.com>
2018-04-08 06:53:56 -0400

    First commit

---
+++ designs.js
@@ -0,0 +1,17 @@
+
+// Select color input
+// Select size input
+
+var picker = $('#colorPicker').val();
+var height = $('#inputHeight');
+var width = $('#inputWeight');
+var pixelCanvas = $('#pixelCanvas');
+var button = $('submitButton');
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(rows, columns) {
+
+// Your code goes here!
+
+}
