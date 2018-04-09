9ac3c991001ca122b93b4a041174e9c98ee5aaba
Kevin Sudor <ksudor42@gmail.com>
2018-04-08 22:08:51 -0400

    added submit button functionality

--- designs.js
+++ designs.js
@@ -1,17 +1,32 @@
+console.log('bonjour');
 
 // Select color input
+var pickedColor = $('#colorPicker').val();
 // Select size input
-
-var picker = $('#colorPicker').val();
-var height = $('#inputHeight');
-var width = $('#inputWeight');
+// var height = $('#inputHeight').val();
+// var width = $('#inputWeight').val();
+// Select canvas
 var pixelCanvas = $('#pixelCanvas');
-var button = $('submitButton');
+// Select submit button
+// var button = $('submitButton');
+
+// Submit button
+$("input[type='submit']").on("click",function(event) {
+
+	event.preventDefault();
+	console.log("LISTEN!");
+
+	const height = $("#inputHeight").val();
+	const width = $('#inputWeight').val();
+
+	makeGrid(height,width);
+});
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid(rows, columns) {
+function makeGrid(height,width) {
 
 // Your code goes here!
+	console.log(height,width);
 
 }
